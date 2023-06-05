6bb248fd59d2965bbd7f4cc338b6ebd3ffa05a4c
Jamie Gaehring <gaehj457@gmail.com>
2023-06-05 14:08:06 -0400

    Watch geotrace pkg in example's Vite config.

--- examples/geotrace/vite.config.js
+++ examples/geotrace/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    watch: {
+      ignored: [
+        '!**/node_modules/farmos-map-geotrace/**'
+      ],
+    },
+  },
   optimizeDeps: {
     exclude: [
       'farmos-map-geotrace',
